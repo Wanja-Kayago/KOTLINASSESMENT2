5088a018b56e74f80fff249852f8a2af9f1427fe
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-03-25 08:23:15 +0300

    added files

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+//QN2
+fun main() {
+    var nums= arrayOf(21,42,11,34,45)
+    println(nums.sum())
+    println(nums.count())
+    println(nums.average())
+}
+fun numbers(numbs:Array<Int>):Array<Int> {
+return numbs
+}
+
+
