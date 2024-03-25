8c47acade98a656c9092258bc0cf4a262aa04fcb
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-03-25 09:06:25 +0300

    added files

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,14 @@ fun main() {
     println(nums.sum())
     println(nums.count())
     println(nums.average())
+
+    var num=calc(50)
 }
 fun numbers(numbs:Array<Int>):Array<Int> {
 return numbs
 }
-
+fun calc(num:Int){
+    var findVolume= 4/3 *3.14159 * num*num*num
+    println(findVolume)
+}
 
